301eff30166de0d3e1ef3eac0b6b830864e2e753
Ханхүү Сүнчин <26lp99264675@administrators-iMac-4.local>
2026-08-26 12:53:11 +0800

    hahah

--- lesson.js
+++ lesson.js
@@ -36,4 +36,8 @@ const numbers = [
   12, -5, 8, 23, -9, 23, 4, -31, 15, -2, 7, -14, 0, 19, -8, 26, -45, 3, -11, -6,
   10, -23, 18, -4, 29, -16, 5, -38, 29,
 ];
-for (let i = 1; i < numbers.length - 1; i++) {}
+for (let i = 1; i < numbers.length - 1; i++) {
+  for (let j = i + 1; j < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+}
